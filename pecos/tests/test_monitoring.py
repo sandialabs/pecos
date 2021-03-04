050ce86ec6ddc04efa9cb7e975fcae3abda09f29
Katherine Klise <kaklise@sandia.gov>
2021-03-04 15:30:26 -0700

    remove debugging

--- pecos/tests/test_monitoring.py
+++ pecos/tests/test_monitoring.py
@@ -105,12 +105,6 @@ class Test_simple_example(unittest.TestCase):
         
         # Object-oriented test
         test_results = self.pm.test_results
-        print()
-        print(self.pm.data)
-        print()
-        print(test_results)
-        print()
-        print(expected)
         assert_frame_equal(test_results, expected, check_dtype=False)
         
         # Functional test
@@ -127,10 +121,6 @@ class Test_simple_example(unittest.TestCase):
         # Object-oriented test
         self.pm.check_missing()
         test_results = self.pm.test_results[self.pm.test_results['Error Flag'] == 'Missing data']
-        print()
-        print(self.pm.test_results)
-        print()
-        print(expected)
         assert_frame_equal(test_results.reset_index(drop=True), expected, check_dtype=False)
         
         # Functional test
