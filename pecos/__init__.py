03136fda73d2066cca665c5fff1e6468429f5119
Katherine Klise <kaklise@sandia.gov>
2023-08-01 11:02:02 -0700

    update version number

--- pecos/__init__.py
+++ pecos/__init__.py
@@ -6,7 +6,7 @@ from pecos import logger
 from pecos import utils
 from pecos import pv
 
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 
 __copyright__ = """Copyright 2016 National Technology & Engineering 
     Solutions of Sandia, LLC (NTESS). Under the terms of Contract 
